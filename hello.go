dc9b0dce580449916ee0c1b8e560b7b85a984242
Daniel X Moore <yahivin@gmail.com>
2014-06-30 13:15:34 -0700

    structs

--- hello.go
+++ hello.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
   fmt.Println("heyy")
+  fmt.Printf("string: %s", "yolo")
+  fmt.Printf("[%d, %d, %d]\n", 1, 2, 3)
+  fmt.Printf("%f\n", 0.99)
 }
---
+++ struct.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type Position struct {
+  x int
+  y int
+}
+
+func main() {
+  pos := Position{}
+
+  fmt.Println(pos)
+}
